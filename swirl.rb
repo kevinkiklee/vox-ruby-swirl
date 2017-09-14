708e26322877046179f4b398fc209b89ba39e46e
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-14 19:37:59 -0400

    Traverse the nodes and wrap the word in a link

--- swirl.rb
+++ swirl.rb
@@ -15,8 +15,48 @@ class Swirl
 
   def money_making_html
     raise 'HTML string is missing' unless @html
-    return @html.to_s unless @db
+    return outputHtmlString unless @db
 
+    @html.traverse do |node|
+      next unless node.is_a? Nokogiri::XML::Text
+      rawWords = node.text.split(' ')
+      words = rawWords.map { |word| word.gsub(/[^a-z0-9]/i, '') }
+
+      words.map! do |word|
+        if @db.has_word?(word.downcase)
+          links = @db.get_links_for_word(word.downcase)
+          linkify_string(word, links)
+        else
+          word
+        end
+      end
+
+      node.content = words.join(' ')
+    end
+    p outputHtmlString
+    outputHtmlString
+  end
+
+  private
+
+  def linkify_string(word, links)
+    link = find_highest_affiliate_amount(links)
+    replace_escaped_characters("<a href='#{link}' target='_blank'>#{word}</a>")
+  end
+
+  def find_highest_affiliate_amount(links)
+    link_with_highest_amount = links.max_by do |link|
+      link[:amount]
+    end
+
+    link_with_highest_amount[:url]
+  end
+  
+  def replace_escaped_characters(string)
+    string.gsub('&lt;', '<').gsub('&gt;', '>')
+  end
+
+  def outputHtmlString
     @html.to_s
   end
 end
