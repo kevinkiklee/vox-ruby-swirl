71519aff91c34fd17a70efb34cbba175d30301d4
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-14 17:20:31 -0400

    Set html and db instance variables

--- swirl.rb
+++ swirl.rb
@@ -3,11 +3,14 @@ class Swirl
   end
 
   def add_html(htmlString)
+    @html = htmlString
   end
 
   def use_affiliate_database(db)
+    @db = db
   end
 
   def money_making_html
+    @html
   end
 end
